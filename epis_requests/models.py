f47777b3733d59b85f1e923a088e99ab44e90640
Davi <davi.ribeirodosanjos@gmail.com>
2023-08-24 16:20:33 -0300

    adicionando solicitacoes

---
+++ epis_requests/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from branches.models import Branches
+from employees.models import Employees
+from epis_requests.models import EPIsRequests
+from epis_sizes.models import EPIsSizes
+
+
+class EPIsRequests(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    date_requested = models.DateField(auto_now_add=True)
+    date_send = models.DateField(null=True)
+    date_confirmed = models.DateField(null=True)
+    date_canceled = models.DateField(null=True)
+    attachment_confirm = models.FileField(
+        upload_to="epis_requests/%Y/%m/%d", blank=True, null=True
+    )
+
+    branch = models.ForeignKey(
+        Branches, on_delete=models.CASCADE, related_name="epis_requests"
+    )
+    employee = models.ForeignKey(
+        Employees, on_delete=models.CASCADE, related_name="epis_requests"
+    )
+    author_create = models.ForeignKey(
+        Employees, on_delete=models.CASCADE, related_name="epis_requests_create"
+    )
+    author_confirm = models.ForeignKey(
+        Employees,
+        on_delete=models.CASCADE,
+        related_name="epis_requests_confirm",
+        null=True,
+    )
+    author_cancel = models.ForeignKey(
+        Employees,
+        on_delete=models.CASCADE,
+        related_name="epis_requests_cancel",
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "EPIRequest"
+        verbose_name_plural = "EPIsRequests"
+        db_table = "epis_requests"
+        app_label = "epis_requests"
+
+    def __repr__(self) -> str:
+        return f"<EPI Request {self.employee.name} - {self.date_requested}>"
+
+    def __str__(self):
+        return f"<EPI Request {self.employee.name} - {self.date_requested}>"
