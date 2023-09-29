dee03e9683b29703d46efa6c3df793269afe76e0
Davi <davi.ribeirodosanjos@gmail.com>
2023-09-29 17:38:06 -0300

    adicionando marketing

--- _service/get_media.py
+++ _service/get_media.py
@@ -5,7 +5,7 @@ import os
 
 def get_media(request, path):
     file_path = os.path.join(settings.MEDIA_ROOT, path)
-    print("aqui")
+
     if os.path.exists(file_path):
         return FileResponse(
             open(file_path, "rb"), content_type="application/octet-stream"
